b80cee4d920f408197fc0f407aaa53a63475676e
inger987 <inger.r.horn@hotmail.no>
2016-01-07 14:08:46 +0100

    knapp til å trykke komme til lyd side

--- app/src/main/java/com/example/inger/beaconbeachler/MainPage.java
+++ app/src/main/java/com/example/inger/beaconbeachler/MainPage.java
@@ -1,16 +1,40 @@
 package com.example.inger.beaconbeachler;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class MainPage extends AppCompatActivity {
+public class MainPage extends AppCompatActivity implements View.OnClickListener {
 
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+    }
+
+    private void Button2Click()
+    {
+        startActivity(new Intent("com.example.inger.beaconbeachler.Lyd_activity"));
+    }
+    @Override
+    public void onClick(View v) {
+    switch (v.getId())
+    {
+        case R.id.button2:
+            Button2Click();
+            break;
+    }
     }
 
     @Override
@@ -34,4 +58,6 @@ public class MainPage extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
