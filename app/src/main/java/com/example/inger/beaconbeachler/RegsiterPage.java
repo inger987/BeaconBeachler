f17a6b0872f002cb5ee668659ac89d04edec05c9
Marielle <marielle.lorentzen@gmail.com>
2016-02-10 13:34:26 +0100

    Ordnet noen bugs i registreringen

--- app/src/main/java/com/example/inger/beaconbeachler/RegsiterPage.java
+++ app/src/main/java/com/example/inger/beaconbeachler/RegsiterPage.java
@@ -1,7 +1,5 @@
 package com.example.inger.beaconbeachler;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,80 +7,89 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class RegsiterPage extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnRegistrer;
-    EditText etRFistname,etRLastname,etRUsername,etRPassword;
-    private static final String REGISTER_URL = "https://home.hbv.no/110118/bachelor/register.php";
+    private static final String REGISTER_URL = "https://home.hbv.no/110118/bachelor/insert.php";
+
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_FIRSTNAME = "firstName";
+    public static final String KEY_LASTNAME = "lastName";
+
+
+    private EditText editTextUsername;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextLastName,editTextFirstName;
+
+    private Button buttonRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regsiter_page);
-        etRFistname = (EditText)findViewById(R.id.etRFirstname);
-        etRLastname = (EditText)findViewById(R.id.etRLastname);
-        etRUsername = (EditText)findViewById(R.id.etRUsername);
-        etRPassword = (EditText)findViewById(R.id.etRPassword);
-        btnRegistrer = (Button)findViewById(R.id.btnRegistrer);
+
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        editTextLastName = (EditText) findViewById(R.id.editTextLastName);
+        editTextFirstName = (EditText) findViewById(R.id.editTextFirstName);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+        buttonRegister.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+        final String username = editTextUsername.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+        final String firstname = editTextFirstName.getText().toString().trim();
+        final String lastname = editTextLastName.getText().toString().trim();
 
 
-        btnRegistrer.setOnClickListener(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(RegsiterPage.this,response,Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(RegsiterPage.this,error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put(KEY_USERNAME,username);
+                params.put(KEY_PASSWORD,password);
+                params.put(KEY_FIRSTNAME, firstname);
+                params.put(KEY_LASTNAME, lastname);
+                return params;
+            }
 
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btnRegistrer:
-                registerUser();
-                break;
+        if(v == buttonRegister){
+            registerUser();
         }
-    }
-    private void registerUser(){
-        String firstName = etRFistname.getText().toString().trim().toLowerCase();
-        String lastName = etRLastname.getText().toString().trim().toLowerCase();
-        String username = etRUsername.getText().toString().trim().toLowerCase();
-        String password = etRPassword.getText().toString().trim().toLowerCase();
-
-        register(firstName,lastName,username,password);
-    }
-
-    private void register(String firstName,String lastName, String username, String password) {
-        class RegisterUser extends AsyncTask<String, Void, String>{
-            ProgressDialog loading;
-            RegisterUserClass ruc = new RegisterUserClass();
-
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-                loading = ProgressDialog.show(RegsiterPage.this, "Please Wait",null, true, true);
-            }
-
-            @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
-                loading.dismiss();
-                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            protected String doInBackground(String... params) {
-
-                HashMap<String, String> data = new HashMap<String,String>();
-                data.put("firstName",params[0]);
-                data.put("lastName",params[1]);
-                data.put("username",params[2]);
-                data.put("password",params[3]);
-
-                String result = ruc.sendPostRequest(REGISTER_URL,data);
-
-                return  result;
-            }
-        }
-
-        RegisterUser ru = new RegisterUser();
-        ru.execute(firstName,lastName,username,password);
     }
 }
--- app/src/main/java/com/example/inger/beaconbeachler/WritingPage.java
+++ app/src/main/java/com/example/inger/beaconbeachler/WritingPage.java
@@ -1,13 +1,32 @@
 package com.example.inger.beaconbeachler;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Marielle on 10-Jan-16.
  */
-public class WritingPage extends AppCompatActivity {
+public class WritingPage extends AppCompatActivity implements View.OnClickListener {
 
+    String url = "https://home.hbv.no/110118/bachelor/insertWriting.php";
+    String item_name;
+
+    EditText item_et;
+    ProgressDialog PD;
+    Button btnSave;
 
 
     @Override
@@ -15,20 +34,57 @@ public class WritingPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writing_page);
 
+        PD = new ProgressDialog(this);
+        PD.setMessage("Loading.....");
+        PD.setCancelable(false);
 
+        item_et = (EditText) findViewById(R.id.etText);
+        btnSave = (Button) findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(this);
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-
-
-
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnSave:
+                insert();
+                break;
+        }
     }
+    public void insert(){
+        PD.show();
+        item_name = item_et.getText().toString();
 
-    @Override
-    public void onStop() {
-        super.onStop();
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        PD.dismiss();
+                        item_et.setText("");
+                        Toast.makeText(getApplicationContext(),
+                                "Data Inserted Successfully",
+                                Toast.LENGTH_SHORT).show();
 
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                PD.dismiss();
+                Toast.makeText(getApplicationContext(),
+                        "failed to insert", Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("item_name", item_name);
+
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        MyApplication.getInstance().addToReqQueue(postRequest);
     }
 }
