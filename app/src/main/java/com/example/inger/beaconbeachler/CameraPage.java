76cbd36fa34210288d319732ba272c9eabda61b1
inger987 <inger.r.horn@hotmail.no>
2016-03-16 14:11:52 +0100

    - Menu - Button design

--- app/src/main/java/com/example/inger/beaconbeachler/CameraPage.java
+++ app/src/main/java/com/example/inger/beaconbeachler/CameraPage.java
@@ -2,6 +2,7 @@ package com.example.inger.beaconbeachler;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -14,10 +15,13 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateFormat;
 import android.util.Base64;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -219,6 +223,109 @@ public class CameraPage extends AppCompatActivity {
 
         UploadImage ui = new UploadImage();
         ui.execute(mBitmap);
+    }
+
+    //Logout function
+    private void logout(){
+        //Creating an alert dialog to confirm logout
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Are you sure you want to logout?");
+        alertDialogBuilder.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                        //Getting out sharedpreferences
+                        SharedPreferences preferences = getSharedPreferences(Config.SHARED_PREF_NAME,Context.MODE_PRIVATE);
+                        //Getting editor
+                        SharedPreferences.Editor editor = preferences.edit();
+
+                        //Puting the value false for loggedin
+                        editor.putBoolean(Config.LOGGEDIN_SHARED_PREF, false);
+
+                        //Putting blank value to email
+                        editor.putString(Config.USERNAME_SHARED_PREF, "");
+
+                        //Saving the sharedpreferences
+                        editor.commit();
+
+                        //Starting login activity
+                        Intent intent = new Intent(CameraPage.this, LoginPage.class);
+                        startActivity(intent);
+                    }
+                });
+
+        alertDialogBuilder.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                    }
+                });
+
+        //Showing the alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+
+    }
+
+    private void showprofil() {
+        //linked to webside /profil
+
+        String url = "https://home.hbv.no/110118/bachelor/homepage/mainPage.php#";
+
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_page, menu);
+       /* --> Try to show icon in overflow       MenuItem item = menu.findItem(R.id.action_sound);
+        SpannableStringBuilder builder = new SpannableStringBuilder("sound.png Login");
+        // replace "*" with icon
+        builder.setSpan(new ImageSpan(this, R.mipmap.sound), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        item.setTitle(builder);*/
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            logout();
+        }
+
+        if (id == R.id.action_favorite) {
+            //se din profil / endre profil
+            showprofil();
+        }
+
+        if (id == R.id.action_sound){
+            startActivity(new Intent(CameraPage.this, Lyd.class));
+        }
+
+        if (id== R.id.action_camera){
+            startActivity(new Intent(CameraPage.this, CameraPage.class));
+        }
+
+        if (id==R.id.action_text){
+            startActivity(new Intent(CameraPage.this, WritingPage.class));
+        }
+
+        if(id==R.id.action_beacon){
+            startActivity(new Intent(CameraPage.this, BeaconPage.class));
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
