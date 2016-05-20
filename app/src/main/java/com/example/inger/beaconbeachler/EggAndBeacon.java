0761e919688fbb2e6d954dc0e69e0083c35ebeb6
Marielle <marielle.lorentzen@gmail.com>
2016-05-20 21:24:29 +0200

    endret hvordan vi henter info fra databasen når det gjelder beacons

--- app/src/main/java/com/example/inger/beaconbeachler/EggAndBeacon.java
+++ app/src/main/java/com/example/inger/beaconbeachler/EggAndBeacon.java
@@ -43,10 +43,7 @@ public class EggAndBeacon extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eggandbeacon);
         Bundle extras = getIntent().getExtras();
-        String uuid = extras.getString("uuid");
-        String id1 = extras.getString("major");
         minor = extras.getString("minor");
-        String ds = extras.getString("distance");
 
         textViewTitle = (TextView) findViewById(R.id.textViewTitle);
         textViewInfo = (TextView) findViewById(R.id.textViewInfo);
@@ -74,59 +71,6 @@ public class EggAndBeacon extends AppCompatActivity {
         };
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
-
-    /*    TextView tekst = new TextView(this);
-        tekst.setTextSize(7 * getResources().getDisplayMetrics().density);
-        tekst.setMovementMethod(new ScrollingMovementMethod());
-
-        overskrift.setTextSize(6 * getResources().getDisplayMetrics().density);
-
-        TextView beacon = (TextView) findViewById(R.id.beacon);
-        beacon.setTextSize(7 * getResources().getDisplayMetrics().density);
-
-        ImageView imageView = (ImageView)findViewById(R.id.bilde);
-        if (id2.equals("1")) {
-            imageView.setImageResource(R.mipmap.vrbriller);
-            overskrift.setText("VIRITUELL VIRKELIGHET");
-            view.setText(uuid + "\n+" + id1 + "\n" + id2 + "\n" + "\n" + ds + "\n" +
-                    "Kunstig virkelighet eller virtuell virkelighet (etter det engelske virtual reality), ofte forkortet VR, " +
-                    "er en datateknologi som lar brukeren påvirke og bli påvirket av et dataskapt miljø som skal etterlikne " +
-                    "en virkelighet. De fleste «miljøene» med kunstig virkelighet omfatter både syns- og lydinntrykk og blir vist" +
-                    " på en skjerm eller gjennom et spesielt, apperat. Det mest kjente apperatet er VR briller (som vist på bildet)." +
-                    " Ved å bruke brillene tilater teknologien brukeren å se seg 360 grader rundt i den dataskapte virkeligheten. ");
-        }
-
-        else if (id2.equals("2")) {
-            imageView.setImageResource(R.mipmap.unhand);
-            overskrift.setText("UNLIMITED HAND");
-            view.setText(uuid +"\n+"+ id1 +"\n"+ id2 +"\n"+ ds +"\n" +
-                    "Dette er en teknologi som kan brukes sammen med VR briller. Du plaserer sensoren rundt armen(som vist på bildet) " +
-                    "og den finner ut hvordan og hvor du beveger hånden. Dette gjør det mulig å bruke hendene når du spiller " +
-                    "med VR-briller. Du kan for eksempel klappe dyr, bygge, bruke skytevåpen og mye mer. Kun fantasien setter grenser.");
-        }
-
-        else if (id2.equals("3")) {
-            imageView.setImageResource(R.mipmap.contactlens);
-            overskrift.setText("GOOGLE KONTAKTLINSER");
-            view.setText(uuid + "\n+" + id1 + "\n" + id2 + "\n" + ds + "\n" +
-                    "Google kontaktliser er en smartteknologi som er spesielt utviklet for personer med diabetes." +
-                    " Den sjekker konstant glukose(sukker) - nivået i tårene. Produktet er enda ikke på markedet, " +
-                    "men under testing.");
-
-        }
-
-        else if (id2.equals("4")) {
-            imageView.setImageResource(R.mipmap.driverless);
-            overskrift.setText("FØRERLØS BIL");
-            view.setText(uuid +"\n+"+ id1 +"\n"+ id2 +"\n"+ ds +"\n" +
-                    "Tenk deg biler uten fører! Ny teknologi, tankegang og forskning gjør dette mulig. " +
-                    "Ved hjelp av kameraer og sensorer som er plassert rundt bilen, leser den trafikkskilt, " +
-                    "veimerkering og andre bilers plassering. \n" +
-                    "Eksperter mener dette er fremtiden, og at det vil føre til færre trafikkulykker. ");
-        } */
-
-
-
     }
 
     private void getJson(String response){
